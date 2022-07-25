3e697c7f7eb3e0fc7e02138b2fb4f171facdb1b5
Pedro Andrade <pedrofreitasdeandrade@gmail.com>
2022-07-25 08:33:43 -0300

    implementar script scrollreveal

---
+++ js/configScroll.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal({
+    distance: '50px',
+    duration: 800,
+    origin: 'top',
+    reset: false
+})
+
+
+// Delay
+
+sr.reveal('.apresentacao', { delay: 200 })
+sr.reveal('.carousel-inner', { delay: 300 })
+sr.reveal('.noticias', { delay: 300 })
+sr.reveal('.final', { delay: 350 })
